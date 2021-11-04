65ebf096bc3a3e7594ce6492d0059efbf8e9cf99
TheMerms <nikmermer@gmail.com>
2021-11-03 21:24:42 -0700

    Create lab.js

---
+++ lab10/js/lab.js
@@ -0,0 +1,17 @@
+/*
+* Author: Nik Mermer
+* Created: October 18
+*/
+
+var make = "Honda";
+var model = "Element";
+var color = "Orange";
+var year = 2009;
+
+var age = 2021 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
